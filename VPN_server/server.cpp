8f22bcce9dcd8014213a46c992c2acddf2811156
zy <769671058@qq.com>
2017-09-29 11:53:30 +0800

    correct bug

--- VPN_server/server.cpp
+++ VPN_server/server.cpp
@@ -26,6 +26,7 @@ uint32_t LOCAL_IP_UINT32;
 
 unordered_map<uint64_t, uint16_t> srcIpPort_mp_localPort;
 unordered_map<uint64_t, sockaddr_in> localPort_mp_srcAddr;
+unordered_map<uint16_t, uint64_t> localPort_mp_clientIpPort;
 
 list<uint16_t> be_use_port;
 
@@ -111,29 +112,31 @@ int main(int argc, char *argv[])
                         local_port = 0;
                         break;
                 }
-                sockaddr_in client_addr = localPort_mp_srcAddr[local_port];
+                sockaddr_in src_addr = localPort_mp_srcAddr[local_port];
+                uint64_t client_ip_port = localPort_mp_clientIpPort[local_port];
 
-                uint32_t src_ip = client_addr.sin_addr.s_addr;
-                uint16_t src_port = client_addr.sin_port;
+                uint32_t client_ip = (client_ip_port >> 32);
+                uint16_t client_port = (client_ip_port & 0xffff);
 
                 cout << "tun0" << endl;
-                cout << "src_ip:" << src_ip << " " << "src_port:" << src_port << endl;
+                cout << "src_ip:" << src_addr.sin_addr.s_addr << " " << "src_port:" << src_addr.sin_port << endl;
+                cout << "client_ip:" << client_ip << " " << "client_port:" << client_port << endl;
 
-                ip_package.dst_addr(IPv4Address(src_ip));
+                ip_package.dst_addr(IPv4Address(client_ip));
                 switch(ip_package.protocol())
                 {
                     case 6:
-                        ip_package.find_pdu<TCP>()->dport(src_port);
+                        ip_package.find_pdu<TCP>()->dport(client_port);
                         break;
                     case 17:
-                        ip_package.find_pdu<UDP>()->dport(src_port);
+                        ip_package.find_pdu<UDP>()->dport(client_port);
                         break;
                     default:
                         break;
                 }
 
                 vector<uint8_t> ip_package_buf = ip_package.serialize();
-                if(udp.sentMsg((char *)ip_package_buf.data(), ip_package_buf.size(), client_addr) <= 0)
+                if(udp.sentMsg((char *)ip_package_buf.data(), ip_package_buf.size(), src_addr) <= 0)
                 {
                     perror("udp.sendMsg");
                     continue;
@@ -157,17 +160,44 @@ int main(int argc, char *argv[])
                     continue;
                 }
 
+                sockaddr_in src_addr = udp.getSockaddr_in();
+                uint32_t src_ip = src_addr.sin_addr.s_addr;
+                uint16_t src_port = src_addr.sin_port;
 
-                sockaddr_in client_addr = udp.getSockaddr_in();
+                uint32_t client_ip;
+                if((client_ip = inet_addr(ip_package.src_addr().to_string().data())) == INADDR_NONE)
+                {
+                    perror("inet_addr");
+                    exit(errno);
+                }
 
-                uint32_t src_ip = client_addr.sin_addr.s_addr;
-                uint16_t src_port = client_addr.sin_port;
+                uint16_t client_port;
+                switch (ip_package.protocol())
+                {
+                    case 6:
+                        client_port = ip_package.find_pdu<TCP>()->sport();
+                        break;
+                    case 17:
+                        client_port = ip_package.find_pdu<UDP>()->sport();
+                        break;
+                    default:
+                        client_port = 0;
+                        break;
+                }
 
                 cout << "udp0" << endl;
                 cout << "src_ip:" << src_ip << " " << "src_port:" << src_port << endl;
+                cout << "client_ip:" << client_ip << " " << "client_port;" << client_port << endl;
 
-                uint64_t src_ip_port = src_ip;
+                if(client_port == 0)
+                {
+                    cout << "PORT = 0!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+                    continue;
+                }
+
+                uint64_t src_ip_port = src_ip, client_ip_port = client_ip;
                 src_ip_port = (src_ip_port << 32) + src_port;
+                client_ip_port = (client_ip_port << 32) + client_port;
 
                 uint64_t local_port;
                 if(srcIpPort_mp_localPort.count(src_ip_port) > 0)
@@ -185,7 +215,8 @@ int main(int argc, char *argv[])
 
                     srcIpPort_mp_localPort[src_ip_port] = local_port;
                 }
-                localPort_mp_srcAddr[local_port] = client_addr;
+                localPort_mp_srcAddr[local_port] = src_addr;
+                localPort_mp_clientIpPort[local_port] = client_ip_port;
 
                 ip_package.src_addr(IPv4Address(TUN0_IP));
                 switch(ip_package.protocol())
